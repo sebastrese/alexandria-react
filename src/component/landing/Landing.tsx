54b0d309fb3b4a133ba5b1c03f151192261e4e52
aruiz <aruiz@fsoc-deb.fsociety>
2020-06-11 19:51:34 -0600

    Finish footer

--- src/component/landing/Landing.tsx
+++ src/component/landing/Landing.tsx
@@ -52,7 +52,7 @@ export default function Landing() {
                     {/** Main title */}
                     <section id='home' className='flex flex-row items-center justify-center h-full w-full max-w-full 
                     flex-wrap-reverse pt-20 lg:flex-row md:pt-32'>
-                        <div className='flex flex-col self- text-lg p-8 pt-4 pb-16 lg:w-3/12'>
+                        <div className='flex flex-col text-lg p-8 pt-4 pb-16 lg:w-3/12'>
                             <span id='heading-text' className='text-5xl font-black break-words whitespace-pre brand-text md:text-6xl'>
                                 Knowledge <br/>
                                 for <br/>
@@ -154,21 +154,22 @@ export default function Landing() {
                     <span className='text-brand font-black text-5xl'>Powered</span>
                     <span className='w-24 mt-4 mb-8' style={{border: '2px solid', borderColor: '#e0e0e0'}} />
                     <div className='flex flex-row flex-wrap w-full justify-center items-center px-16'>
-                        <Amazon className='w-full h-auto md:w-3/12 lg:w-1/6' />
-                        <Google className='w-full h-auto my-8 md:py-0 md:mx-8 md:w-3/12 lg:w-1/6' />
-                        <Spotify className='w-full h-auto md:w-3/12 lg:w-1/6' />
+                        <Amazon className='w-9/12 h-auto md:w-3/12 lg:w-1/6' />
+                        <Google className='w-9/12 h-auto my-8 md:py-0 md:mx-8 md:w-3/12 lg:w-1/6' />
+                        <Spotify className='w-9/12 h-auto md:w-3/12 lg:w-1/6' />
                     </div>
                 </section>
 
                 {/** Footer */}
                 <section id='about' className='footer flex flex-col w-full'>
-                    <div className='flex flex-col flex-wrap px-16 py-12 w-full'>
+                    <div className='flex flex-col flex-wrap px-16 py-12 w-full lg:px-32'>
                         <div className='flex flex-row items-center'>
                             <span className='h-12 w-12 rounded-full mr-3' id='logo' />
-                            <a href='/landing' className='font-black text-2xl brand-text'>Alexandria</a>
+                            <a href='#home' className='font-black text-2xl brand-text'>Alexandria</a>
                         </div>
+
                         <div className='flex flex-row flex-wrap'>
-                            <div className='flex flex-row my-8'>
+                            <div className='flex flex-row my-8 mr-8 lg:mr-16'>
                                 <div>
                                     <span className='text-brand text-xl'><b className='font-extrabold'>Explore</b></span>
                                     <ul className='text-lg'>
@@ -178,23 +179,86 @@ export default function Landing() {
                                     </ul>
                                 </div>
                             </div>
-                            <div className='flex flex-row my-8'>
+
+                            <div className='flex flex-row my-8 md:mr-8 lg:mr-16'>
                                 <div>
-                                    <span className='text-brand text-xl'><b className='font-extrabold'>Explore</b></span>
+                                    <span className='text-brand text-xl'><b className='font-extrabold'>Get Involved</b></span>
                                     <ul className='text-lg'>
-                                        <li className='mt-4'><a href='#home'>Home</a></li>
-                                        <li className='my-2'><a href='#why'>Why</a></li>
-                                        <li><a href='#features'>Features</a></li>
+                                        <li className='mt-4'><Link to='/donations'>Donate</Link></li>
+                                        <li className='my-2'><a href='mailto:jobs@damascus-engineering.com?subject=Alexandria%20job%20application'>Careers</a></li>
                                     </ul>
                                 </div>
                             </div>
+
+                            <div className='flex flex-row my-8 mr-8 lg:mr-16'>
+                                <div>
+                                    <span className='text-brand text-xl'><b className='font-extrabold'>Social</b></span>
+                                    <ul className='flex flex-row flex-wrap text-lg'>
+                                        <li className='mr-4'>
+                                            <a href='https://facebook.com/alexandria-project'>
+                                                <span className='mdi mdi-facebook text-4xl' />
+                                            </a>
+                                        </li>
+                                        <li className='mr-4'>
+                                            <a href='https://twitter.com/alexandria-project'>
+                                                <span className='mdi mdi-twitter text-4xl' />
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href='https://instagram.com/alexandria-project'>
+                                                <span className='mdi mdi-instagram text-4xl' />
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <span className='hidden md:flex md:flex-auto' />
+
+                            <div className='flex flex-row my-8 mr-8 lg:mr-16'>
+                                <div>
+                                    <span className='text-brand text-xl'><b className='font-extrabold'>Contact</b></span>
+                                    <ul className='flex flex-row flex-wrap text-lg'>
+                                        <li className='mr-4'>
+                                            <a href='mailto:contact@damascus-engineering.com'>
+                                                <span className='mdi mdi-email text-4xl' />
+                                            </a>
+                                        </li>
+                                        <li className='mr-4'>
+                                            <a href='tel:+018005890000'>
+                                                <span className='mdi mdi-phone text-4xl' />
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div className='flex flex-row my-8'>
+                                <div>
+                                    <span className='text-brand text-xl'><b className='font-extrabold'>More</b></span>
+                                    <ul className='flex flex-row flex-wrap items-center text-lg'>
+                                        <li className='mr-4'>
+                                            <Link to='/country' className='flex flex-row items-center'>
+                                                <span className='mdi mdi-earth text-4xl mr-1' />
+                                                <span className='font-black text-sm'>US</span>
+                                            </Link>
+                                        </li>
+                                        <li className='mr-4'>
+                                        <Button type='icon' className='flex mx-4 stroke-default p-2 md:hidden' action={() => changeTheme()}>
+                                            <span className='material-icons'>brightness_4</span>
+                                        </Button>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
-                    <div className='flex flex-col flex-wrap text-center w-full px-16 py-10 bg-indigo md:flex-row'>
+                    <div className='flex flex-col flex-wrap text-center w-full px-16 py-10 bg-indigo md:flex-row lg:px-32'>
                         <span><b className='font-black'>The Alexandria Foundation</b> © 2020 · All rights reserved</span>
                         <span className='flex-auto' />
                         <Link className='my-4 md:my-0 md:mx-8' to='/privacy'>Privacy Policy</Link>
-                        <Link to='/service'>Terms of Service</Link>
+                        <Link to='/services'>Terms of Service</Link>
                     </div>
                 </section>
             </div>
