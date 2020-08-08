0b6e89cdde2608a0e6730c3fe89241a05ffa4d99
Sebastián C <sebastrese@outlook.com>
2020-08-07 20:52:00 -0600

    added props to media player

--- src/component/mediaPlayer/Player.tsx
+++ src/component/mediaPlayer/Player.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { Button } from "../../shared/component/Button";
 import { Link } from "react-router-dom";
-function Player() {
+import PropTypes from "prop-types";
+
+/**
+ * @param {string} props.title title
+ * @param {string} props.autor autor
+ * @param {string} props.pic background pic
+ */
+
+function Player(props: IProps) {
   return (
     <div
       className="grid grid-cols-3  viewport bg-black items-center animate__animated animate__fadeIn animate__faster 
     lg:py-4 lg:px-8"
     >
-      <div className="flex content-end flex-wrap col-span-1 border border-teal-400 w-full h-full lg:max-w-screen-xl">
+      <div className="flex content-end flex-wrap col-span-1 w-full h-full lg:max-w-screen-xl">
         <div className=" flex flex-col p-6">
-          <span className="pt-6 ">nombre</span>
-          <span className="pt-6">autor</span>
+          <span className="font-sans text-3xl pt-6 ">{props.title}</span>
+          <span className="font-sans text-sm pt-6">{props.autor}</span>
           <div className="pt-6">
-            <span>botones</span>
+            <div className=" flex flex-row space-x-6 font-bold">
+              <button className="rounded-full py-2 px-4 items-center border border-gray-800">
+                Games
+              </button>
+              <button className="rounded-full py-2 px-4 items-center border border-gray-800">
+                Art
+              </button>
+            </div>
           </div>
           <div className="flex justify-center pt-6">
             <Link to="/" className="flex items-center">
@@ -27,13 +42,23 @@ function Player() {
         </div>
       </div>
 
-      <div className="col-span-2 border bg-red-700 border-teal-400  w-full h-full lg:max-w-screen-xl">
-        <img
-          src="https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/H2x1_NSwitch_Minecraft_image1600w.jpg"
-          className="h-full w-full object-cover"
-        ></img>
+      <div className="col-span-2 bg-red-700 w-full h-full lg:max-w-screen-xl">
+        <img src={props.pic} className="h-full w-full object-cover"></img>
       </div>
     </div>
   );
 }
+
+interface IProps {
+  title: string;
+  autor: string;
+  pic: string;
+}
+
+Player.propTypes = {
+  title: PropTypes.string,
+  autor: PropTypes.string,
+  pic: PropTypes.string,
+};
+
 export default Player;
